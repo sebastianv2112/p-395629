0d61e944ad688f56aa464be21bab42e121c45836
sebastianv2112 <sebastianv2112@gmail.com>
2025-03-11 15:00:52 -0500

    Cambios GTM

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,26 @@
+import React from "react";
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+// Agregar Google Tag Manager (GTM)
+const gtmId = "GTM-WWHNL4W5"; // Reemplaza con tu ID real
+
+(function (w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != "dataLayer" ? "&l=" + l : "";
+  j.async = true;
+  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, "script", "dataLayer", gtmId);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
